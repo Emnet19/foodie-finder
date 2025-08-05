e4f9dc703acd47f3ddf3634559c9bf56465d0f78
fenet firomsa <fenetfiromsa2012@gmail.com>
2025-08-05 21:13:46 +0300

    Add random meal generator page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  Home  from "./pages/Home"; 
+import RandomMeal from "./pages/RandomMeal";
 
 function App() {
-
   return (
-    <>
-     <Home/>
-    </>
-  )
+    <Router>
+      
+      <Routes>
+        <Route path="/random" element={<RandomMeal />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
